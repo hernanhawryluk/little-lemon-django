b46489039effa7f149798f1a2f0f70bc394bf917
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-02-17 13:00:04 -0300

    Added unittest

---
+++ restaurant/tests/test_views.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from restaurant.views import MenuItemsView
+from rest_framework.test import RequestsClient
+
+client = RequestsClient()
+
+class MenuItemsView(TestCase):
+    def test_menu_response(self):
+        client = RequestsClient()
+        response = client.get('http://127.0.0.1:8000/restaurant/menu-items')
+        assert response.status_code == 200
+
+    def test_menu_item(self):
+        client = RequestsClient()
+        response = client.get('http://127.0.0.1:8000/restaurant/menu-items/1')
+        self.assertEqual(
+            response.data,
+            {'id': 1, 'title': 'PanCake', 'price': '1.50', 'inventory': 10},
+        )

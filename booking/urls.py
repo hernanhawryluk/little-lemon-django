0fb147316976c9959e79e184767a7c86555b82be
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-02-27 16:17:53 -0300

    Added edit and delete views and templates

--- booking/urls.py
+++ booking/urls.py
@@ -9,5 +9,8 @@ from . import views
 
 urlpatterns = [
     path('booking/', login_required(views.booking_view), name='booking'),
+    path('booking/success/<int:pk>', login_required(views.successful_booking_view), name='successful_booking'),
+    path('booking/edit/<int:pk>', login_required(views.edit_booking_view), name='edit_booking'),
+    path('booking/delete/<int:pk>', login_required(views.delete_booking_view), name='delete_booking'),
     # path('booking/', include(router.urls)),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,33 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from .models import Booking
 from .forms import BookingForm
 from .serializers import BookingSerializer
 from rest_framework import viewsets, permissions
 from django.core.exceptions import ValidationError
+from datetime import datetime
 
 # Create your views here.
 
 def booking_view(request):
-    errors = None
-    form = BookingForm()
-    bookings = Booking.objects.all()
-
     if request.method == 'POST':
         try:
             form = BookingForm(request.POST)
             new_booking = form.save(commit=False)
             new_booking.user = request.user
             new_booking.save()
+            return redirect('successful_booking', pk=new_booking.pk)
         except ValidationError as e:
             errors = e.message_dict
         except ValueError:
             errors = 'Please provide valid data'
         except:
             errors = 'Something went wrong'
+        return redirect('booking')
 
-    return render(request, 'booking.html', {'form': BookingForm, 'errors': errors, 'bookings': bookings})
+    if request.method == 'GET':
+        errors = None
+        form = BookingForm()
+        bookings = Booking.objects.filter(user=request.user, date__gte=datetime.now().date()).order_by('date', 'time')
+        return render(request, 'booking.html', {'form': BookingForm, 'errors': errors, 'bookings': bookings})
     
+def successful_booking_view(request, pk):
+    booking = get_object_or_404(Booking, pk=pk, user=request.user)
+    return render(request, 'success.html', {'booking': booking})
+
+def edit_booking_view(request, pk):
+    booking = get_object_or_404(Booking, pk=pk, user=request.user)
+    
+    if request.method == 'GET':
+        errors = None
+        form = BookingForm(instance=booking)
+        bookings = Booking.objects.filter(user=request.user, date__gte=datetime.now().date()).exclude(pk=pk).order_by('date', 'time')
+        ordered_bookings = [booking] + list(bookings)
+        return render(request, 'booking.html', {'form': form, 'errors': errors, 'bookings': ordered_bookings, 'editing': pk})
+    
+    if request.method == 'POST':
+        if booking.user == request.user:
+            try:
+                form = BookingForm(request.POST, instance=booking)
+                form.save()
+                return redirect('booking')
+            except ValidationError as e:
+                errors = e.message_dict
+            except ValueError:
+                errors = 'Please provide valid data'
+            except:
+                errors = 'Something went wrong'
+            return render(request, 'booking.html', {'form': form, 'errors': errors, 'bookings': bookings, 'editing': pk})
+        
+def delete_booking_view(request, pk):
+    booking = get_object_or_404(Booking, pk=pk, user=request.user)
+    if request.method == 'POST':
+        if(booking.user == request.user):
+            booking.delete()
+            return redirect('booking')
+    
+    return render(request, 'cancelation.html', {'booking': booking})
+
 # class BookingViewSet(viewsets.ModelViewSet):
 #     queryset = Booking.objects.all()
 #     serializer_class = BookingSerializer

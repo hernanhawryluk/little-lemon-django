ad45adc84f0806c4ae36417c1953c7a1bb983875
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-03-07 14:25:08 -0300

    Added test units for Booking API

--- booking/tests.py
+++ booking/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from .models import Booking
 from .forms import BookingForm
 from datetime import datetime, timedelta
+from rest_framework.test import RequestsClient
 
 # Create your tests here.
 class TestBookingSetUp(TestUserSetUp):
@@ -93,3 +94,33 @@ class TestBookingViews(TestBookingSetUp):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(Booking.objects.count(), 0)
 
+
+class BookingListCreateViewTest(TestBookingSetUp):
+    client = RequestsClient()
+
+    def test_get_all_reservations_without_credentials(self):
+        response = self.client.get('http://127.0.0.1:8000/api/booking/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 0)
+
+    def test_get_all_reservations_with_credentials(self):
+        response = self.client.get('http://127.0.0.1:8000/api/booking/', headers={'Authorization': f'Token {self.token}'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+
+    def test_create_a_reservation_without_credentials(self):
+        response = self.client.post('http://127.0.0.1:8000/api/booking/', data=self.reservation)
+        self.assertEqual(response.status_code, 403)
+
+    def test_create_a_reservation_with_credentials(self):
+        response = self.client.post('http://127.0.0.1:8000/api/booking/', headers={'Authorization': f'Token {self.token}'}, data=self.reservation)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Booking.objects.count(), 2)
+
+    def test_edit_a_reservation(self):
+        response = self.client.get(f'http://127.0.0.1:8000/api/booking/{self.reservation_1.pk}/', headers={'Authorization': f'Token {self.token}'}, data=self.reservation)
+        self.assertEqual(response.status_code, 200)
+
+    def delete_a_reservation(self):
+        response = self.client.delete(f'http://127.0.0.1:8000/api/booking/{self.reservation_1.pk}/', headers={'Authorization': f'Token {self.token}'})
+        self.assertEqual(response.status_code, 204)

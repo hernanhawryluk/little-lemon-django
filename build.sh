9e7cba76c55de37319708441625dcd83e837151b
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-03-08 13:49:44 -0300

    Trying to fix the DB

--- build.sh
+++ build.sh
@@ -5,5 +5,10 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+# python manage.py migrate
 
+# temp for fixing migrations
+python manage.py migrate booking zero
+rm -f booking/migrations/*
+python manage.py makemigrations booking
+python manage.py migrate

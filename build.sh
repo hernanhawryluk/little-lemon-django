0c43cbd58d972268016e945e22344aef133ea81b
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-03-08 14:31:44 -0300

    Changes for deploy

--- build.sh
+++ build.sh
@@ -5,8 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-# python manage.py migrate
-
-# temp for fixing migrations
-rm -rf src/booking/migrations
 python manage.py migrate

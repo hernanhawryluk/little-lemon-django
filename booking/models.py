e91f93d06ad2a0af54880471ad6d77b8baf06499
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-03-08 16:36:32 -0300

    Changes for deploy

--- booking/models.py
+++ booking/models.py
@@ -8,25 +8,6 @@ User = get_user_model()
 
 # Create your models here.
 class Booking(models.Model):
-    def validate_date(value):
-        today = datetime.now().date()
-        six_months_from_now = today + timedelta(days=180)
-
-        if value < today:
-            raise ValidationError('Date must be in the future')
-        elif value > six_months_from_now:
-            raise ValidationError('Date must be within 6 months')
-
-    def validate_time(value):
-        if value < datetime.now().time():
-            raise ValidationError('Time must be in the future')
-        
-    def validate_guests(value):
-        if value < 1:
-            raise ValidationError('Guests must be at least 1')
-        elif value > 12:
-            raise ValidationError('Guests must be at most 12')
-    
     occasion_choices = [
         ('Casual', 'Casual'),
         ('Birthday', 'Birthday'),
@@ -34,12 +15,29 @@ class Booking(models.Model):
     ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField(validators=[validate_date])
-    time = models.TimeField(validators=[validate_time])
-    guests = models.SmallIntegerField(validators=[validate_guests] , default = 1)
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.SmallIntegerField(default = 1)
     occasion = models.CharField(max_length = 12, choices=occasion_choices , default = 'Casual')
     created = models.DateTimeField(auto_now_add = True)
     updated = models.DateTimeField(auto_now = True)
 
     def __str__(self) -> str:
         return f'{self.user.username} booked {self.guests} seats on {self.date} for {self.time}'
+    
+    def clean(self):
+        super().clean()
+
+        today = datetime.now().date()
+        if self.date == today and self.time < datetime.now().time():
+            raise ValidationError({'time': 'Time must be in the future'})
+        
+        if self.date < today:
+            raise ValidationError({'date': 'Date must be in the future'})
+        elif self.date > today + timedelta(days=180):
+            raise ValidationError({'date': 'Date must be within 6 months'})
+
+        if self.guests < 1:
+            raise ValidationError({'guests': 'Guests must be at least 1'})
+        elif self.guests > 12:
+            raise ValidationError({'guests': 'Guests must be at most 12'})

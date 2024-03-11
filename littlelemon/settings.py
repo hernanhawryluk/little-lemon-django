9cfb686962daef74aa566c44014745f3ef90ff27
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-03-11 14:03:20 -0300

    Another try to deploy

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -140,16 +140,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets')
-]
 
-if DEBUG: #if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'assets')
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

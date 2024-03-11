5f4ead36f28389e49b232f8dba3afd88dabf6596
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-03-11 17:59:38 -0300

    A new attempt on Deploying

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,11 @@
+echo "BUILD START"
+
+python3.9 -m venv venv
+source venv/bin/activate
+
 pip3 install -r requirements.txt
+
+echo "BUILD END"
+
+echo "Collecting static files..."
 python3.9 manage.py collectstatic
